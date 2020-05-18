a2dc1cb6ee4d52ed42f97489c4e650fe80e310a0
tvrcgo <tvrcgo@gmail.com>
2020-05-18 16:56:12 +0800

    update layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import styles from './index.less'
+import Link from 'umi/link'
 
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.layout}>
       <div className={styles.header}>
         <div className={styles.lt}>
-          <h1 className={styles.title}>app</h1>
+          <h1 className={styles.title}>
+            <Link to='/'>App</Link>
+          </h1>
         </div>
         <div className={styles.rt}>
-          <span className={styles.profile}>username</span>
+          <div className={styles.profile}>
+            <span>region</span>
+            <span>lang</span>
+            <span>username</span>
+          </div>
         </div>
       </div>
-      <div className={styles.main}>
+      <div className={styles.body}>
         <div className={styles.sider}>
 
         </div>

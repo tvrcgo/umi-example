8959f0edf51afb5d9f94e5d16bcdb2bb0cf481c2
tvrcgo <tvrcgo@gmail.com>
2020-05-21 14:29:57 +0800

    404 page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,8 +14,8 @@ export default (props: any) => {
         width: '400px'
       }}>
         <h1>Page not found</h1>
-        <div style={{ color: '#999' }}>{props.location.pathname}</div>
-        <div style={{ marginTop: 50 }}><Link to='/'><Button type='primary'>Go home</Button></Link></div>
+        <div style={{ color: '#999', fontFamily: 'verdana, sans-serif' }}>{props.location.pathname}</div>
+        <div style={{ marginTop: 50, fontFamily: 'verdana, sans-serif' }}><Link to='/'><Button type='primary'>Go home</Button></Link></div>
       </div>
     </div>
   )
